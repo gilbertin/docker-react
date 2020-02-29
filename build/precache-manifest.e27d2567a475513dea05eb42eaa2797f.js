91946b4d13dc8196165b15a922c48ea26c89513b
Gilberto Morales <gilberto@MacBook-Pro-de-Gilberto.local>
2020-02-29 01:58:19 -0600

    second commit COPY files

---
+++ build/precache-manifest.e27d2567a475513dea05eb42eaa2797f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0082943773d67156dec3f198f2f29bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1710995032551c405916",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7078c5e2808bcb857495",
+    "url": "/static/js/2.b4ec4706.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b4ec4706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1710995032551c405916",
+    "url": "/static/js/main.8d8c77bf.chunk.js"
+  },
+  {
+    "revision": "691443989d219c585434",
+    "url": "/static/js/runtime-main.1eb18e34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
